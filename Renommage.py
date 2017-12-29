b80f8339e04efafd78e0a1f3ea22f8815ca1ed42
vmorin95 <34586462+vmorin95@users.noreply.github.com>
2017-12-29 23:45:47 +0100

    Update Renommage.py

--- Renommage.py
+++ Renommage.py
@@ -1,16 +1,20 @@
 from Action import *
-import os
 
-class Renommage (Action) :
+# Classe utilisée pour renommer les fichiers dans le système
+
+class Renommer(Action):
     #constructeur
-    def __init__(self, nomdurepertoire, regle):
-        Action.__init__(self,nomdurepertoire,regle)
-    #méthode renommer fichier
-    def rename_files(self):
-        original, renommer = self.simule()
+    def __init__(self, nomrepertoire, regle):
+        Action.__init__(self, nomrepertoire, regle)
+
+    # Méthodes
+
+    #renommer les fichiers
+    def renommer_fichiers(self):
+        original, renommer = self.simulation()
         j = 0
         for i in range(len(original)):
-            os.rename(os.path.join(self.nomdurepertoire, original[i]),
-                      os.path.join(self.nomdurepertoire, renommer[i]))
+            os.rename(os.path.join(self.nomrepertoire, original[i]),
+                      os.path.join(self.nomrepertoire, renommer[i]))
             j += 1
         return j
